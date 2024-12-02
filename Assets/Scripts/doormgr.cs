c4af40dbb2a6476d1b64997ff5d302f011799738
simondatking school <69970561+simondatking@users.noreply.github.com>
2024-12-02 13:14:10 +0100

    sync

--- Assets/Scripts/doormgr.cs
+++ Assets/Scripts/doormgr.cs
@@ -27,13 +27,14 @@ public class Doormgr : MonoBehaviour
     {
         
     }
-    void Switched_area(Scene prev,Scene next)
+    public void Switched_area(Scene prev,Scene next)
     {
         GameObject[] roots = next.GetRootGameObjects();
         GameObject grid = roots.First((obj) => { return obj.name == "Grid"; });
         Door_pointer door_pointer = grid.GetComponent<Door_pointer>();
         Vector3 spawn = door_pointer.Spawn_point.transform.position;
         player.transform.position = new Vector3(spawn.x, spawn.y, spawn.z);
+        player_camera.transform.position = new Vector3(spawn.x, spawn.y, spawn.z);
         PolygonCollider2D PolygonCollider = door_pointer.Camera_Collider.GetComponent<PolygonCollider2D>();
         CinemachineConfiner2D CinemachineConfiner= player_camera.GetComponent<CinemachineConfiner2D>();
         CinemachineConfiner.BoundingShape2D = PolygonCollider;
